d1b96ca6bfde34d7dda7c0331a125545e39ffab5
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-09-18 14:49:50 +0000

    My Mini Emoji Project

---
+++ app.js
@@ -0,0 +1,19 @@
+const closedFace = document.querySelector('.closed');
+const openedFace = document.querySelector('.open');
+
+//to add an event listener
+closedFace.addEventListener('click', () => {
+    if (openedFace.classList.contains('open'))
+    {
+        openedFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+
+openedFace.addEventListener('click', () => {
+    if (closedFace.classList.contains('closed'))
+    {
+        closedFace.classList.add('active');
+        openedFace.classList.remove('active');
+    }
+});
